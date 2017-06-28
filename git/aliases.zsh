ff5a94a8df242e8612d43b5aedae2b53fd1eaf75
John Rees <john@bitsushi.com>
2017-06-28 21:25:20 +0100

    aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -21,3 +21,5 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+
+alias trimbranches='git branch --merged | grep -v "\*" | grep -v master | grep -v dev | xargs -n 1 git branch -d'
