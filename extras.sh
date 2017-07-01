3d5770a81f05c55924484b4a0716aa9ae8dcb40c
John Rees <john@bitsushi.com>
2017-07-01 22:24:46 +0100

    Sublime stuff

--- extras.sh
+++ extras.sh
@@ -1,2 +1,5 @@
 # Install latest rbenv
 rbenv install `rbenv install -l | grep -v - | tail -1`
+
+# Link Sublime Prefs
+ln -s ~/.dotfiles/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
