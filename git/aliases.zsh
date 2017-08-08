cdb62e480ce2ec89478a5be5970c7389c4cc8472
John Rees <john@bitsushi.com>
2017-08-08 16:51:08 +0100

    Add gprb alias (git pull --rebase origin master)

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -21,5 +21,6 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias gprb='git pull --rebase origin master'
 
 alias trimbranches='git branch --merged | grep -v "\*" | grep -v master | grep -v dev | xargs -n 1 git branch -d'
